f3a30fe04f3e8d377c8209f0ace5a7ad3db558c7
Mikko De Los Reyes <mikkode212@gmail.com>
2024-10-22 10:07:51 -0400

    created promo and net inputs

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ function App() {
     <div className="container">
       <h1>Tip Out Calculator</h1>
       <div className="form-group">
-        <label>Total Sales</label>
+        <label>Total Sales: </label>
         <input
         type='number'
         placeholder="Enter total sales"
+        />        
+      </div>
+      <div className="form-group">
+        <label>Promos (comma separated - e.g. 10,10,53.50): </label>
+        <input
+        type='text'
+        placeholder="Enter promo amounts"
         />
-        
+      </div>
+      <div className="form-group">
+        <label>Net Amount: </label>
+        <input
+        type='number'
+        placeholder="Enter NET amount"
+        />
       </div>
     </div>
   );
